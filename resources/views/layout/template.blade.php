749a4edf6976991c4319edfa2464780fa19ac235
kabiryusufbashir <kabiryusufbashir@gmail.com>
2024-01-09 19:04:18 +0100

    Push Update

--- resources/views/layout/template.blade.php
+++ resources/views/layout/template.blade.php
@@ -26,7 +26,7 @@
 
         @yield('pageMeta')
 
-        <link rel="stylesheet" href="{{ asset('build/assets/app-2408e937.css') }}">
+        <link rel="stylesheet" href="{{ asset('build/assets/app-9464c8f8.css') }}">
         <link href="{{ asset('css/main.css?v=1.1') }}" rel="stylesheet">
         @vite('resources/css/app.css')
     
